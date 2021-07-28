8551b49035090dab749b22035dad8b5a783b919b
glamboyosa <ogbemudiatimothy@gmail.com>
2021-07-28 14:22:06 +0100

    feat: update tutorial and images

--- App.js
+++ App.js
@@ -10,8 +10,10 @@ import {
   Dimensions,
   TouchableOpacity,
   Alert,
+  Image,
 } from 'react-native';
 import auth from '@react-native-firebase/auth';
+import LinearGradient from 'react-native-linear-gradient';
 
 const App = () => {
   // Replace `URL` below with LocalTunnel URL in the format : https://{subdomain}.loca.lt
@@ -31,8 +33,6 @@ const App = () => {
   };
 
   const onPressHandler = async () => {
-    setLoading(true);
-
     try {
       const body = {phone_number: phoneNumber};
       console.log('tru.ID: Creating SIMCheck for', body);
@@ -99,50 +99,72 @@ const App = () => {
   };
 
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.heading}>tru.ID + Firebase Auth</Text>
-      <View style={styles.form}>
-        {sentCode ? (
-          <View>
-            <TextInput
-              style={styles.textInput}
-              placeholder="OTP"
-              placeholderTextColor="#d3d3d3"
-              onChangeText={text => setCode(text)}
-              value={code}
-            />
-            {loading ? (
-              <ActivityIndicator size="large" color="#00ff00" />
-            ) : (
-              <TouchableOpacity
-                onPress={confirmationHandler}
-                style={styles.button}>
-                <Text style={styles.buttonText}>Verify</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-        ) : (
-          <View>
-            <TextInput
-              style={styles.textInput}
-              keyboardType="phone-pad"
-              placeholder="ex. +448023432345"
-              placeholderTextColor="#d3d3d3"
-              onChangeText={text => setPhoneNumber(text.replace(/\s+/g, ''))}
-            />
+    <LinearGradient
+      colors={['rgba(25, 85, 255, 40)', 'rgba(10, 10, 50, 66)']}
+      useAngle={true}
+      angle={0}
+      style={{
+        flex: 1,
+      }}>
+      <SafeAreaView style={styles.backgroundStyle}>
+        <StatusBar barStyle="light-content" />
+        <View style={styles.box}>
+          <Image
+            style={styles.logo}
+            source={require('./images/tru-logo.png')}
+          />
+          <Text style={styles.heading}>tru.ID + Firebase Auth</Text>
+          {sentCode ? (
+            <View style={styles.center}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="OTP"
+                placeholderTextColor="#d3d3d3"
+                onChangeText={text => setCode(text)}
+                value={code}
+              />
+              {loading ? (
+                <ActivityIndicator
+                  style={styles.spinner}
+                  size="large"
+                  color="#00ff00"
+                />
+              ) : (
+                <TouchableOpacity
+                  onPress={confirmationHandler}
+                  style={styles.button}>
+                  <Text style={styles.buttonText}>Verify</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          ) : (
+            <View style={styles.center}>
+              <TextInput
+                style={styles.textInput}
+                keyboardType="phone-pad"
+                placeholder="ex. +448023432345"
+                placeholderTextColor="#d3d3d3"
+                onChangeText={text => setPhoneNumber(text.replace(/\s+/g, ''))}
+              />
 
-            {loading ? (
-              <ActivityIndicator size="large" color="#00ff00" />
-            ) : (
-              <TouchableOpacity onPress={onPressHandler} style={styles.button}>
-                <Text style={styles.buttonText}>Login</Text>
-              </TouchableOpacity>
-            )}
-          </View>
-        )}
-      </View>
-    </SafeAreaView>
+              {loading ? (
+                <ActivityIndicator
+                  style={styles.spinner}
+                  size="large"
+                  color="#00ff00"
+                />
+              ) : (
+                <TouchableOpacity
+                  onPress={onPressHandler}
+                  style={styles.button}>
+                  <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+              )}
+            </View>
+          )}
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
@@ -151,7 +173,28 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#ffffff',
+  },
+  center: {
+    alignItems: 'center',
+  },
+  box: {
+    width: '90%',
+    borderRadius: 3,
+    backgroundColor: '#fff',
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: {width: 0.5, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 0.7 * Dimensions.get('window').height,
+    padding: 15,
+  },
+  logo: {
+    marginTop: 10,
+    width: 0.5 * Dimensions.get('window').width,
+    height: 200,
   },
   heading: {
     fontSize: 50,
@@ -163,30 +206,41 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button: {
-    elevation: 8,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#e67e22',
-    color: '#ffffff',
+    backgroundColor: '#1955ff',
+    color: '#fff',
     paddingVertical: 10,
     paddingHorizontal: 12,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#1955ff',
+    marginTop: 17,
+    width: '40%',
+  },
+  buttonText: {
+    color: '#fff',
   },
   paragraph: {
     fontSize: 12,
     borderBottomColor: '#e67e22',
   },
-  buttonText: {
-    color: '#ffffff',
-  },
   textInput: {
     padding: 15,
-    borderColor: '#20232a',
-    borderWidth: 3,
-    elevation: 7,
-    height: 50,
+    borderRadius: 3,
     backgroundColor: '#fff',
-    marginBottom: 10,
-    width: 0.8 * Dimensions.get('window').width,
+    borderColor: '#858585',
+    borderWidth: 0.4,
+    elevation: 7,
+    shadowColor: '#858585',
+    shadowOffset: {width: 0.5, height: 1},
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    color: '#000',
+    width: 0.7 * Dimensions.get('window').width,
+  },
+  spinner: {
+    marginTop: 20,
   },
 });
 
